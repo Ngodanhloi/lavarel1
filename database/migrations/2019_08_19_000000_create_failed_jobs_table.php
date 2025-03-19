c0405e77d07807aa500c0743558d0d8472ccb68a
loi <ng.du2020@gmail.com>
2025-03-19 22:16:57 +0700

    first commit

---
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid',100)->unique();
+            $table->text('connection',255);
+            $table->text('queue',255);
+            $table->longText('payload',255);
+            $table->longText('exception',255);
+            $table->timestamp('failed_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('failed_jobs');
+    }
+};
